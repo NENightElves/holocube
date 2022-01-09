7b48230e7099ba2922656f5133f9caddb92b713d
NENightElves <NENightElves@qq.com>
2022-01-09 11:06:26 +0000

    update ntp

--- loop.py
+++ loop.py
@@ -14,16 +14,16 @@ def zfill(str, n):
 
 def drawdatetime(datetime):
     dateString = zfill(str(datetime[1]), 2)+' - '+zfill(str(datetime[2]), 2)
-    timeString = zfill(str(datetime[4]), 2)+' : '+zfill(str(datetime[5]), 2)
+    timeString = zfill(str(datetime[3]), 2)+' : '+zfill(str(datetime[4]), 2)
     tft.fill_rect(0, 0, X, Y, st7789.BLACK)
     tft.draw(gothita, dateString, 43, 60, st7789.WHITE, 1.1)
     tft.draw(gothita, timeString, 15, 130, st7789.WHITE, 1.7)
 
 
 def loop():
-    drawdatetime(timeutil.getdatetime())
+    drawdatetime(timeutil.getdatetime(8))
     while True:
-        datetime = timeutil.getdatetime()
-        if datetime[6] == 0:
+        datetime = timeutil.getdatetime(8)
+        if datetime[5] == 0:
             drawdatetime(datetime)
         utime.sleep(1)
--- timeutil.py
+++ timeutil.py
@@ -1,7 +1,9 @@
 import ntptime
+import utime
 from machine import RTC
 
 ntptime.settime()
 
-def getdatetime():
-    return RTC().datetime()
+
+def getdatetime(utc_shift):
+    return utime.localtime(utime.mktime(utime.localtime())+utc_shift*3600)
